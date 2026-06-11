52779135fb3da7ea3347dbcfa8466f1b749efb52
GDK1995 <87063068+GDK1995@users.noreply.github.com>
2026-06-11 16:51:36 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from google import genai
-from google.ai import types
 import os
+from google import genai
+from google.genai import types
 
 st.title("Тестирование Чат-бота 🤖")
 
